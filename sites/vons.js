5766391f4de2153247cda00fb3b1f8f5ec5dac5a
Jared K <jaredkotoff@users.noreply.github.com>
2020-09-23 13:03:03 -0700

    Vons updated coupon button text

--- sites/vons.js
+++ sites/vons.js
@@ -7,8 +7,8 @@ const {
   write,
   button,
   closeBrowser,
-} = require("taiko");
-const site = "Vons";
+} = require('taiko');
+const site = 'Vons';
 
 module.exports = async (email, password) => {
   await (async () => {
@@ -23,34 +23,34 @@ module.exports = async (email, password) => {
     try {
       await openBrowser();
       await goto(
-        "https://www.vons.com/account/sign-in.html?r=https%3A%2F%2Fwww.vons.com%2Fjustforu%2Fcoupons-deals.html&goto=/justforu/coupons-deals.html",
+        'https://www.vons.com/account/sign-in.html?r=https%3A%2F%2Fwww.vons.com%2Fjustforu%2Fcoupons-deals.html&goto=/justforu/coupons-deals.html',
       );
-      await write(email, into(textBox(below("Email"))));
-      await write(password, into(textBox(below("Password"))));
-      await click($("#btnSignIn"));
+      await write(email, into(textBox(below('Email'))));
+      await write(password, into(textBox(below('Password'))));
+      await click($('#btnSignIn'));
       await waitFor(5000);
-      if (await $("#error-message").exists()) throw new Error("invalidLogin");
-      if (await $(".create-modal-close-icon").exists())
-        await click($(".create-modal-close-icon"));
+      if (await $('#error-message').exists()) throw new Error('invalidLogin');
+      if (await $('.create-modal-close-icon').exists())
+        await click($('.create-modal-close-icon'));
       await waitFor(2000);
       let couponsClicked = 0;
       let loadMore = true;
       while (loadMore) {
         try {
-          console.log("Clicking coupons...");
+          console.log('Clicking coupons...');
           let moreCoupons = true;
           while (moreCoupons) {
             try {
-              await click(button("Add"));
+              await click(button('Clip Coupon'));
               couponsClicked++;
             } catch (err) {
               moreCoupons = false;
             }
           }
-          console.log("Loading more coupons...");
-          await click(button("Load more"));
+          console.log('Loading more coupons...');
+          await click(button('Load more'));
         } catch (err) {
-          console.log("Done clicking all coupons!");
+          console.log('Done clicking all coupons!');
           console.log(
             `${couponsClicked} new coupons were added to your ${site} account`,
           );
@@ -58,8 +58,8 @@ module.exports = async (email, password) => {
         }
       }
     } catch (error) {
-      if (error.message.startsWith("invalidLogin")) {
-        console.error("Invalid email or password");
+      if (error.message.startsWith('invalidLogin')) {
+        console.error('Invalid email or password');
       } else {
         console.error(error);
       }
