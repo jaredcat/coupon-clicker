2da87c16d4594b8096c1f1bbd0dc820ab1b74171
Jared.K <3268576+jaredkotoff@users.noreply.github.com>
2023-12-19 18:20:31 -0800

    Fix honeygain

--- src/sites/honeygain.ts
+++ src/sites/honeygain.ts
@@ -7,7 +7,7 @@ import Singletons from '../models/singletons.model';
 
 const name = 'Honeygain';
 const loginUrl = 'https://dashboard.honeygain.com/login';
-const loginButton = "//button[contains(., 'Login')]";
+const loginButton = "//button[contains(., 'Continue with email')]";
 
 const cookiesButton = "//button[contains(., 'Accept selected')]";
 const luckyPotButtonXPath = "//button[contains(., 'Open Lucky Pot')]";
@@ -43,25 +43,28 @@ async function clipCoupons(singletons: Singletons): Promise<number> {
   let couponsClicked = 0;
   let [exists] = await clickOnXPath(page, luckyPotButtonXPath);
   if (exists) {
+    await logger.screenshot(page, 'Lucky pot is available!');
     const [claimed] = await clickOnXPath(page, "//button[contains(., 'OK')]", {
       timeout: 10000,
     });
     if (claimed) {
       couponsClicked++;
-      console.log('Daily bonus claimed!');
+      await logger.screenshot(page, 'Lucky pot claimed');
+      logger.info('Daily bonus claimed!');
     } else {
-      console.error('Error claiming bonus');
+      logger.error('Error claiming bonus');
     }
   } else {
-    console.log('Daily bonus already clicked');
+    await logger.screenshot(page, 'Lucky pot already claimed');
+    logger.info('Daily bonus already clicked');
   }
 
   do {
-    console.log('Checking for any rewards to claim');
+    logger.info('Checking for any rewards to claim');
     [exists] = await clickOnXPath(page, rewardButtonXPath, { timeout: 1000 });
     if (exists) {
       couponsClicked++;
-      console.log('A reward claimed!');
+      logger.info('A reward claimed!');
     }
   } while (exists);
 
@@ -81,12 +84,14 @@ async function login(
   });
   await clearSessionStorage(page);
 
+  await logger.screenshot(page, 'Looking for login');
   await page.waitForSelector('input[name=email]');
 
   await clickOnXPath(page, cookiesButton, { timeout: 100 });
 
   await page.type('input[name=email]', email);
   await page.type('input[name=password]', password);
+  await logger.screenshot(page, 'Login information');
 
   const [button] = await page.$x(loginButton);
   if (button) {
